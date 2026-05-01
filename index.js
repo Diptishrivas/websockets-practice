1315d7cbd340766fa6da6b0479e9daaf3319b7d0
Diptishrivas <Shrivasdipti85@gmail.com>
2026-05-01 21:25:56 +0530

    status update

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import express from 'express';
 import path from 'node:path';
 import {Server, Socket} from 'socket.io'
 
+const CHECKBOX_SIZE = 200;
+const state = {
+  checkboxes: new Array(CHECKBOX_SIZE).fill(false),
+}
+
 async function main(){
     const app = express();
     const server = http.createServer(app);
@@ -26,6 +31,10 @@ async function main(){
 
     app.get('/health',(req,res)=> res.json({healthy: true}));
 
+    app.get('/checkboxes', (req,res)=>{
+
+    return res.json({checkboxes: state.checkboxes});
+    });
 
   server.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
